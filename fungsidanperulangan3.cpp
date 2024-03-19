398f8011d6462b4cc4b241b5711a566d90631b27
SASCHADDD <saschadanu23@gmail.com>
2024-03-19 19:52:57 +0700

    contoh3

---
+++ fungsidanperulangan3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(
+    {
+        int bilangan;
+
+        srand(time(0));
+
+        bilangan = rand() % 10;
+
+        cout << "Bilangan awal = " << bilangan << endl;
+       
+)
