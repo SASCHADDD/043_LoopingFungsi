3993d7385e28f2321d1d088d35b70152cd68bb67
SASCHADDD <saschadanu23@gmail.com>
2024-03-19 19:38:53 +0700

    contoh2

--- Fungsidanprosedur2.cpp
+++ Fungsidanprosedur2.cpp
@@ -2,18 +2,11 @@
 using namespace std;
 
 int main(){
-
     int bilangan;
-    
+
     srand(time(0));
 
-    bilangan = rand()%10;
-}
-while ( bilangan > 4 )
-{
-    cout << "Bilangan lebih dari 4" << endl;
-    bilangan = rand() %10;
-    cout << "Bilangannya ="<< bilangan << endl;
-}
+    bilangan = rand() % 10;
 
-cout << "Bilangan terakhir =" <<bilangan << endl;
+    while (bilangan > 4)
+    {
