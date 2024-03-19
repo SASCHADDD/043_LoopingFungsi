44f4fb0b12769877dd736e619a1c4b783e94cd52
SASCHADDD <saschadanu23@gmail.com>
2024-03-19 20:27:05 +0700

    contoh4

--- fungsidanperulangan4.cpp
+++ fungsidanperulangan4.cpp
@@ -4,3 +4,15 @@ using namespace std;
 int nTelor, nMie, nAir;
 int htelor = 2000, hMie = 2800, hAir = 3000;
 string nama;
+
+void input()
+{
+    cout << "Masukan Nama = ";
+    cin >> nama;
+    cout << "Masukan Jumlah Telor = ";
+    cin >> nTelor;
+    cout << "Masukan Jumlah Mie = ";
+    cin >> nMie;
+    cout << "Masukan Jumlah Air Mineral = ";
+    cin >> nAir;
+}
