a65ca93273c6000c18fcd8cae3fd1df54c6080cf
SASCHADDD <saschadanu23@gmail.com>
2024-03-19 20:16:02 +0700

    contoh3

--- fungsidanperulangan3.cpp
+++ fungsidanperulangan3.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(
+{
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    cout << "Bilangan awal = " << bilangan << endl;
+    do
     {
-        int bilangan;
-
-        srand(time(0));
-
-        bilangan = rand() % 10;
-
-        cout << "Bilangan awal = " << bilangan << endl;
-        do
-        {
-            cout << "Perulangan DO..WHILE" << endl;
-            bilangan = rand() % 10;
-            cout << "Bilangannya = " << bilangan << endl;
-        } while (bilangan > 5);  
-    }
-)
